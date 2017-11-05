c715f79ef0b439c070d759cd66ca3301f20633c0
john T <johnnyta92@gmail.com>
2017-11-05 14:06:31 -0800

    push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('#timer').hide();
+	$('#section').hide();
 });
 
 $('.content').on('click',function(){
@@ -7,45 +8,45 @@ $('.content').on('click',function(){
 })
 
 var questions = [{
-	questions:'In their D and D adventures, which one of the 4 friends is the wizard',
+	questions:'1) In their D and D adventures, which one of the 4 friends is the wizard',
 	answers:['A) Will', 'B) Dustin','C) Eleven','D) Lucas'],
 	correctAnswer:'A) Will'
 },
 {
-	questions:' What is the name of the city that the story takes place in?',
+	questions:'2) What is the name of the city that the story takes place in?',
 	answers:['A) Hawkins ', 'B) Springfield','C) Winterfell','D) Salt Lake City'],
 	correctAnswer: 'A) Hawkins'
 },
 {
-	questions:' What department of the government is the lab running under?',
+	questions:'3) What department of the government is the lab running under?',
 	answers:['A) US Dept of Agriculture', 'B) Federal Drug Administration','C) US Dept of Energy','D) US Dept of Forestry'],
 	correctAnswer: 'A) US Dept of Agriculture'
 },
 {
-	questions:' What was Dr. Brenner looking for when experimenting on 11',
+	questions:'4) What was Dr. Brenner looking for when experimenting on 11',
 	answers:['A) Telekinesis', 'B) His Daughter','C) Russin Intellegince','D) Will'],
 	correctAnswer: 'A) Russin Intellegince'
 },
 {
-	questions:' What is 11’s favorite food',
+	questions:"5) What is Eleven\’s favorite food",
 	answers:['A) Hot Cheetos and Takis', 'B) Eggo Waffles','C) Hot Dogs','D) Salami'],
 	correctAnswer: 'A) Eggo Waffles'
 },
 
 {
-	questions:' How does will communicate with joyce while in the upside down?',
+	questions:'6) How does will communicate with joyce while in the upside down?',
 	answers:['A) Walkie Talkie', 'B) Christmas Lights','C) Texting','D) Playstation'],
 	correctAnswer: 'A) Christmas Lights'
 },
 
 {
-	questions:' What is the domogorgon eating when nancy spots it in the Upside Down?',
+	questions:'7) What is the domogorgon eating when nancy spots it in the Upside Down?',
 	answers:['A) A Deer', 'B) Barb','C) Eleven','D) A Dog'],
 	correctAnswer: 'A) A Deer'
 },
 
 {
-	questions:' What is the domogorgon eating when nancy spots it in the Upside Down?',
+	questions:'8) What is the domogorgon eating when nancy spots it in the Upside Down?',
 	answers:['A) A Baseball Bat', 'B) Glock 19','C) Shotgun','D) AWP'],
 	correctAnswer: 'A) A Baseball Bat'
 }];
@@ -55,7 +56,7 @@ var game = {
 	correct : 0,
 	incorrect :0,
 	unanswered :0,
-	number :50,
+	number :10,
 
 	countdown: function(){
 		game.number--;
@@ -77,69 +78,69 @@ var game = {
 		for (var i = 0; i < questions.length; i++) {
 			$('#section').append('<h3>' + questions[i].questions+ '</h3><br>');
 			for (var j = 0; j < questions[i].answers.length; j++) {
-				$('#section').append("<input type='radio' name='question-"+i+"' value='"+ questions[i].answers[j]+"'>"+questions[i].answers[j])
+				$('#section').append("<input type='radio' name='question-"+i+"' value='"+ questions[i].answers[j]+"'>"+'<br>'+questions[i].answers[j])
 			}
 		}
 	},
 
 	done: function() {
-		$.each($('input[name="questions-0]":checked'),function(){
+		$.each($('input[name="questions-0"]:checked'),function(){
 			if($(this).val()==questions[0].correctAnswer){
 				game.correct++;
 			} else {
 				game.incorrect++;
 			}
 		});
-		$.each($('input[name="questions-1]":checked'),function(){
+		$.each($('input[name="questions-1"]:checked'),function(){
 			if($(this).val()==questions[1].correctAnswer){
 				game.correct++;
 			} else {
 				game.incorrect++;
 			}
 		});
-		$.each($('input[name="questions-2]":checked'),function(){
+		$.each($('input[name="questions-2"]:checked'),function(){
 			if($(this).val()==questions[2].correctAnswer){
 				game.correct++;
 			} else {
 				game.incorrect++;
 			}
 		});
-		$.each($('input[name="questions-3]":checked'),function(){
+		$.each($('input[name="questions-3"]:checked'),function(){
 			if($(this).val()==questions[3].correctAnswer){
 				game.correct++;
 			} else {
 				game.incorrect++;
 			}
 		});
-		$.each($('input[name="questions-4]":checked'),function(){
+		$.each($('input[name="questions-4"]:checked'),function(){
 			if($(this).val()==questions[4].correctAnswer){
 				game.correct++;
 			} else {
 				game.incorrect++;
 			}
 		});
-		$.each($('input[name="questions-5]":checked'),function(){
+		$.each($('input[name="questions-5"]:checked'),function(){
 			if($(this).val()==questions[5].correctAnswer){
 				game.correct++;
 			} else {
 				game.incorrect++;
 			}
 		});
-		$.each($('input[name="questions-6]":checked'),function(){
+		$.each($('input[name="questions-6"]:checked'),function(){
 			if($(this).val()==questions[6].correctAnswer){
 				game.correct++;
 			} else {
 				game.incorrect++;
 			}
 		});
-		$.each($('input[name="questions-7]":checked'),function(){
+		$.each($('input[name="questions-7"]:checked'),function(){
 			if($(this).val()==questions[7].correctAnswer){
 				game.correct++;
 			} else {
 				game.incorrect++;
 			}
 		});
-		$.each($('input[name="questions-8]":checked'),function(){
+		$.each($('input[name="questions-8"]:checked'),function(){
 			if($(this).val()==questions[8].correctAnswer){
 				game.correct++;
 			} else {
@@ -150,11 +151,11 @@ var game = {
 	},
 
 	result: function(){
-		clearinterval(timer);
+		clearInterval(timer);
 		$('#timer').hide();
 		$('#section').html('<h1>'+'Correct: ' + this.correct + '</h1><br>' + 
 			'<h1>'+'Incorrect: ' + this.incorrect + '</h1><br>' +
-			'<h1>'+'Unanswered: ' +(questions.length(this.incorrect+this.correct))+ '</h1><br>')
+			'<h1>'+'Unanswered: ' +(questions.length-(this.incorrect+this.correct))+ '</h1><br>')
 	}
 }
 
